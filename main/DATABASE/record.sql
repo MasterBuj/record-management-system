0d05dc282849104a99580007692658a4da3dddb7
MasterBuj <jhanmail47@gmail.com>
2021-11-16 14:17:30 +0800

    second commit

--- main/DATABASE/record.sql
+++ main/DATABASE/record.sql
@@ -1,20 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 19, 2018 at 05:44 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: Nov 16, 2021 at 07:13 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `record`
@@ -26,26 +27,29 @@ SET time_zone = "+00:00";
 -- Table structure for table `doc_type`
 --
 
-CREATE TABLE IF NOT EXISTS `doc_type` (
-`id` int(11) NOT NULL,
+CREATE TABLE `doc_type` (
+  `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `doc_type`
 --
 
 INSERT INTO `doc_type` (`id`, `name`) VALUES
+(44, 'AAA'),
 (14, 'Certificate'),
-(15, 'Invoice'),
-(16, 'Proposal'),
 (17, 'Contract'),
-(18, 'Report'),
 (19, 'Guarantee'),
+(15, 'Invoice'),
 (20, 'License'),
+(27, 'Memo'),
+(52, 'Notice'),
 (21, 'Passport'),
-(22, 'Warrant'),
+(16, 'Proposal'),
+(18, 'Report'),
 (23, 'Spread Sheet'),
+(22, 'Warrant'),
 (24, 'White Paper');
 
 -- --------------------------------------------------------
@@ -54,21 +58,22 @@ INSERT INTO `doc_type` (`id`, `name`) VALUES
 -- Table structure for table `offices`
 --
 
-CREATE TABLE IF NOT EXISTS `offices` (
-`id` int(11) NOT NULL,
+CREATE TABLE `offices` (
+  `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `offices`
 --
 
 INSERT INTO `offices` (`id`, `name`) VALUES
-(1, 'Dream Host'),
-(2, 'Xcon Services'),
-(3, 'Casper'),
+(7, 'Casper'),
 (4, 'CP Digital'),
-(5, 'Pokse Inc.');
+(1, 'Dream Host'),
+(10, 'NAKS Inc.'),
+(5, 'Pokse Inc.'),
+(2, 'Xcon Services');
 
 -- --------------------------------------------------------
 
@@ -76,8 +81,8 @@ INSERT INTO `offices` (`id`, `name`) VALUES
 -- Table structure for table `transaction`
 --
 
-CREATE TABLE IF NOT EXISTS `transaction` (
-`id` int(11) NOT NULL,
+CREATE TABLE `transaction` (
+  `id` int(11) NOT NULL,
   `date` varchar(100) NOT NULL,
   `doc_type` varchar(100) NOT NULL,
   `description` varchar(1000) NOT NULL,
@@ -86,7 +91,7 @@ CREATE TABLE IF NOT EXISTS `transaction` (
   `dateout` varchar(100) NOT NULL,
   `receive_by` varchar(100) NOT NULL,
   `ft` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `transaction`
@@ -99,8 +104,7 @@ INSERT INTO `transaction` (`id`, `date`, `doc_type`, `description`, `office`, `s
 (4, '05/22/2018', 'Certificate', 'Certificate of . .', 'CP Digital', 'Completed', '08/09/2018', 'Bruno Den', 'James William'),
 (5, '02/01/2018', 'White Paper', 'Giving information on an issue', 'Casper', 'Needs to View', '03/22/2018', 'Logan Paul', 'Elijah'),
 (6, '04/09/2018', 'Spread Sheet', 'Analysis and Storage of data', 'CP Digital', 'Delivered', '04/13/2018', 'Benjamin', 'James William'),
-(7, '07/07/2018', 'Proposal', 'Written offer to buyer', 'Xcon Services', 'Declined', '07/09/2018', 'Ethan', 'John Doe'),
-(8, '10/02/2018', 'Contract', 'Agreement for . .', 'Pokse Inc.', 'Needs to Sign', '10/19/2018', 'Harry Den', 'Ryan Carter');
+(7, '07/07/2018', 'Proposal', 'Written offer to buyer', 'Xcon Services', 'Declined', '07/09/2018', 'Ethan', 'John Doe');
 
 -- --------------------------------------------------------
 
@@ -108,18 +112,19 @@ INSERT INTO `transaction` (`id`, `date`, `doc_type`, `description`, `office`, `s
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-`id` int(11) NOT NULL,
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `password` varchar(100) NOT NULL,
+  `admin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`) VALUES
-(1, 'admin', 'admin');
+INSERT INTO `user` (`id`, `username`, `password`, `admin`) VALUES
+(1, 'admin', 'admin', 1);
 
 --
 -- Indexes for dumped tables
@@ -129,25 +134,27 @@ INSERT INTO `user` (`id`, `username`, `password`) VALUES
 -- Indexes for table `doc_type`
 --
 ALTER TABLE `doc_type`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- Indexes for table `offices`
 --
 ALTER TABLE `offices`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- Indexes for table `transaction`
 --
 ALTER TABLE `transaction`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -157,22 +164,27 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `doc_type`
 --
 ALTER TABLE `doc_type`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+
 --
 -- AUTO_INCREMENT for table `offices`
 --
 ALTER TABLE `offices`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
 --
 -- AUTO_INCREMENT for table `transaction`
 --
 ALTER TABLE `transaction`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
